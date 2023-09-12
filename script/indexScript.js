1b68d31259008ee7781137f08a494845079a9691
pedritossj7 <pedritossj7@gmail.com>
2023-09-12 13:19:07 +0200

    12/9/2023 #1

--- script/indexScript.js
+++ script/indexScript.js
@@ -6,36 +6,11 @@ window.onload = function() {
     let section2 = document.getElementById("section2");
     let section3 = document.getElementById("section3");
 
-    section1.addEventListener("click", function() {
-        section2.style.display = "none";
-        section3.style.display = "none";
-
-        main.style.gridTemplateAreas = "'uno'";
-
-        section1.addEventListener("click", function() {
-            location.reload();
-        });
-    });
-
     section2.addEventListener("click", function() {
-        section1.style.display = "none";
-        section3.style.display = "none";
-
-        main.style.gridTemplateAreas = "'dos'";
-
-        section2.addEventListener("click", function() {
-            location.reload();
-        });
+        window.location = "experience.html";
     });
 
     section3.addEventListener("click", function() {
-        section1.style.display = "none";
-        section2.style.display = "none";
-
-        main.style.gridTemplateAreas = "'tres'";
-
-        section3.addEventListener("click", function() {
-            location.reload();
-        });
+        window.location = "aboutMe.html";
     });
 }
