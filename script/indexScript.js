1478046929205b9cbbf042a5a62fc152001a09b3
pedritossj7 <pedritossj7@gmail.com>
2023-09-13 13:18:37 +0200

    13/9/2023 #1

--- script/indexScript.js
+++ script/indexScript.js
@@ -6,11 +6,19 @@ window.onload = function() {
     let section2 = document.getElementById("section2");
     let section3 = document.getElementById("section3");
 
+    let loadingScreen = document.getElementById("loadingScreen");
+
     section2.addEventListener("click", function() {
-        window.location = "experience.html";
+        loadingScreen.style.animation = "loadingSection 1.5s ease-in forwards";
+        setTimeout(function() {
+            window.location = "experience.html";
+        }, 1000);
     });
 
     section3.addEventListener("click", function() {
-        window.location = "aboutMe.html";
+        loadingScreen.style.animation = "loadingSection 1.5s ease-in forwards";
+        setTimeout(function() {
+            window.location = "aboutMe.html";
+        }, 1000);
     });
 }
